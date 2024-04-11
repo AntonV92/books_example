96ea84309325217732e77d08a4cb99e30ecd4f45
AntonV92 <av476347@gmail.com>
2024-04-11 10:40:42 +0300

    prepare resource/controller

---
+++ app/Models/Book.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $author
+ * @property string $pub_date
+ * @property string $genre
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Book extends Model
+{
+    use HasFactory;
+}
---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-primary text-white">Add New Book</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('books.store') }}">
+                        @csrf
+
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control" id="title" name="title" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="author">Author</label>
+                            <input type="text" class="form-control" id="author" name="author" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="pub_date">Publication Date</label>
+                            <input type="date" class="form-control" id="pub_date" name="pub_date" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="genre">Genre</label>
+                            <input type="text" class="form-control" id="genre" name="genre" required>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Add Book</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
